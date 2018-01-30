055b7a6c9dc74e05d41ae572c1bdb7f49f899997
Phily Austria <Phily.Austria@ff.com>
2018-01-29 23:24:10 -0800

    example(Promise all) : Added bluebird package.

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import chalk from 'chalk';
 import fetch from 'node-fetch';
+import Promise from 'bluebird';
 
 const vorpal = require('vorpal')();
 
@@ -27,6 +28,35 @@ vorpal
     console.log(chalk.green(JSON.stringify(body)));
 
     callback();
+  });
+
+// Play around with Promise all
+vorpal
+  .command('testPromiseAll <user>', 'Get user')
+  .action(async (args, callback) => {
+    let res = await fetch(`https://api.github.com/users/${args.user}`);
+    const body = await res.json();
+
+    if (body.message==="Not Found") {
+      console.log(chalk.red(JSON.stringify(body)));
+    }
+
+    res = await fetch(body.followers_url);
+    const followers = await res.json();
+
+    const promise = followers.map(async (user) => {
+      const url = `https://api.github.com/users/${user.login}`;
+      const res = await fetch(url);
+      const body = await res.json();
+
+      return body;
+    });
+
+    await Promise.all(promise).then((followerInfo) => {
+      console.log(followerInfo);
+      callback();
+    });
+
   });
 
 vorpal
